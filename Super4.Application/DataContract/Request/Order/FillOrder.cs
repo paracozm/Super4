d535c25d4e8a0c8644bbd2fb34f26bda5fdb1642
Diego Brinker <diego.brinker@gmail.com>
2023-01-03 13:24:42 -0300

    FillOrder class implementation

---
+++ Super4.Application/DataContract/Request/Order/FillOrder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Super4.Application.DataContract.Request.Order
+{
+    internal class FillOrder
+    {
+    }
+}
